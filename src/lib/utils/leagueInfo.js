3cec0b7255acc03a9178f95116e2a29ae665469f
KSmith-16 <35820290+KSmith-16@users.noreply.github.com>
2023-06-16 13:22:47 -0400

    Update leagueInfo.js
    Finished ethan update

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -83,7 +83,7 @@ export const managers = [
          image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
        },
        "favoritePlayer": 240, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
+       "valuePosition": "TE", // (optional) Favorite position (QB, WR, RB, TE, etc.)
        "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
        "philosophy": "Next year will be my year", // (optional)
        "tradingScale": 10, // 1 - 10 (optional)
